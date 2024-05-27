38be70dd251feea29e0aa8f33f7ddfc60041643a
Fernando Josué Montano <fernando.montano41@gmail.com>
2024-05-27 14:37:40 -0600

    fix(patch): added deployment patch

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,12 @@ dotenv.config();
 
 app.use(cors());
 app.options("*", cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 
 export default app;
